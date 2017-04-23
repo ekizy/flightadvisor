79b839cc72f79b8b8876ff0773ce1590db6eac37
serkanbekir <bekir@itu.edu.tr>
2017-04-23 14:05:45 +0300

    Access token added.

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 var http = require('http');
 
+app.get('/webhook/', function (req, res) {
+    if (req.query['hub.verify_token'] === 'EAAaIdyjALlgBAGP83GpOMB8PCPxqqq1fsXyExWd69K5ug5Yh6muvD5FV8H3Rsam8AvuUETNAUbNXzDpPu9z' +
+        'Co4z4OlIzlXj95qehoR5OF00aT0qHekHk5j6VKahjZC3bvnFf8HRo3KKTPhrsGc8jEI5e0ZCzXLGiMER8T6ngZDZD') {
+        res.send(req.query['hub.challenge'])
+    } else {
+        res.send('Error, wrong token')
+    }
+})
+
 http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
