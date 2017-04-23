6a5756c26990b5ae86067f8bc7e3b7c0245aac83
ekizy <ekizy@itu.edu.tr>
2017-04-23 14:12:53 +0300

    Token is added

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
+
 var http = require('http');
+
+app.get('/webhook/', function (req, res) {
+    if (req.query['hub.verify_token'] === 'EAAaIdyjALlgBAGP83GpOMB8PCPxqqq1fsXyExWd69K5ug5Yh6muvD5FV8H3Rsam8AvuUETNAUbNXzDpPu9zCo4z4OlIzlXj95qehoR5OF00aT0qHekHk5j6VKahjZC3bvnFf8HRo3KKTPhrsGc8jEI5e0ZCzXLGiMER8T6ngZDZD') {
+        res.send(req.query['hub.challenge'])
+    } else {
+        res.send('Error, wrong token')
+    }
+})
 
 http.createServer(function (req, res) {
     
