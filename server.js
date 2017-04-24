26ca8a9925e39bf01b833d9fb50caaddbf287813
ekizy <ekizy@itu.edu.tr>
2017-04-24 18:35:24 +0300

    Initial commit for Travis

--- server.js
+++ server.js
@@ -218,7 +218,7 @@ function callSendAPI(messageData) {
             console.log("Successfully sent generic message with id %s to recipient %s",
                 messageId, recipientId);
         } else {
-            console.error("Unable to send message.");
+            console.error("Unable to send message");
             console.error(response);
             console.error(error);
         }
